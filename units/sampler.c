43d3da1e87332c81ad7faa001319cc3120c140eb
David Konsumer <konsumer@jetboystudio.com>
2024-12-17 16:09:02 -0800

    use defines to make code clearer

--- units/sampler.c
+++ units/sampler.c
@@ -5,7 +5,10 @@
 
 #include "null-unit.h"
 
-// params: pitch, sampleID
+#define PARAM_PITCH 0
+#define PARAM_SAMPLE 1
+#define PARAM_LAST 1
+
 unsigned int params[2];
 
 // this holds the same loaded from host (0-255 position float)
@@ -15,14 +18,13 @@ unsigned int sample[1024] = {};
 // called when the unit is loaded
 void init(unsigned int initialParams[2]) {
   if (initialParams != NULL) {
-    params[0] = initialParams[0];
-    params[1] = initialParams[1];
+    params[PARAM_PITCH] = initialParams[PARAM_PITCH];
+    params[PARAM_SAMPLE] = initialParams[PARAM_SAMPLE];
   } else {
-    params[0] = 440;
-    params[1] = 0;
+    params[PARAM_PITCH] = 440;
+    params[PARAM_SAMPLE] = 0;
   }
-  get_bytes(params[1], 0, 1024, sample);
-
+  get_bytes(params[PARAM_SAMPLE], 0, 1024, sample);
   show_params(params);
 }
 
@@ -39,8 +41,8 @@ unsigned int get_param_count() {
 // returns the name of the parameter (32 characters, max)
 char* get_name_param(unsigned int param) {
   switch(param) {
-    case 0: return "pitch";
-    case 1: return "sample ID";
+    case PARAM_PITCH: return "pitch";
+    case PARAM_SAMPLE: return "sample ID";
     default: return NULL;
   }
 }
@@ -55,29 +57,34 @@ void destroy() {}
 
 // process a single value, in a 0-255 position frame, return output
 float process(unsigned char position, float input, unsigned char channel) {
-    float scaledPos = (position * 4.0f * params[0]) / 440.0f;
+    float scaledPos = (position * 4.0f * params[PARAM_PITCH]) / 440.0f;
     unsigned int samplePos = (unsigned int)scaledPos % 1024;
+
     char o[20];
     itoa(sample[samplePos], o);
     trace(o);
+
     return *((float*)&sample[samplePos]);
 }
 
 // set a parameter
 void param_set(unsigned int param, unsigned int value) {
-  if (param > 2) {
+  if (param > PARAM_LAST) {
     return;
   }
   params[param] = value;
-  if (param == 1 && value != params[1]) {
-    get_bytes(params[1], 0, 1024, sample);
+
+  // load sample form host if it changed
+  if (param == PARAM_SAMPLE && value != params[PARAM_SAMPLE]) {
+    get_bytes(params[PARAM_SAMPLE], 0, 1024, sample);
   }
+
   show_params(params);
 }
 
 // get the current value of a parameter
 unsigned int param_get(unsigned int param) {
-  if (param > 2) {
+  if (param > PARAM_LAST) {
     return 0;
   }
   return params[param];
