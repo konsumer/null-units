28585b49638d6f301e471cc979bdf611dc85f6e8
David Konsumer <konsumer@jetboystudio.com>
2024-12-29 22:15:38 -0800

    improving form

--- docs/unit-types/UnitWasm.js
+++ docs/unit-types/UnitWasm.js
@@ -2,6 +2,11 @@
 
 import genUI from '../gen-ui.js'
 
+// enum from units
+const NULL_PARAM_BOOL = 0
+const NULL_PARAM_I32 = 1
+const NULL_PARAM_F32 = 2
+
 export default class UnitWasm {
   constructor(manager, name, dirUnits) {
     this.manager = manager
@@ -53,20 +58,21 @@ export default class UnitWasm {
     if (this.params[pi]) {
       this.params[pi].value = value
 
-      // this seems a bit broke
-      // if (this.params[pi].input){
-      //   if (this.params[pi].input.type === 'checkbox') {
-      //     if (this.params[pi].input.checked != value) {
-      //       this.params[pi].input.checked = value
-      //       this.params[pi].input.dispatchEvent(new Event('change'))
-      //     }
-      //   } else {
-      //     if (this.params[pi].input.value != value) {
-      //       this.params[pi].input.value = value
-      //       this.params[pi].input.dispatchEvent(new Event('change'))
-      //     }
-      //   }
-      // }
+      // TODO: float inputs still jump badly on input
+      if (this.params[pi].input) {
+        if (this.params[pi].type === NULL_PARAM_BOOL && this.params[pi].input.checked != !!value) {
+          this.params[pi].input.checked = !!value
+          this.params[pi].input.dispatchEvent(new Event('change'))
+        }
+        if (this.params[pi].type === NULL_PARAM_I32 && parseInt(this.params[pi].input.value) !== parseInt(value)) {
+          this.params[pi].input.value = parseInt(value).toString()
+          this.params[pi].input.dispatchEvent(new Event('change'))
+        }
+        if (this.params[pi].type === NULL_PARAM_F32 && parseFloat(this.params[pi].input.value).toFixed(4) !== parseFloat(value).toFixed(4)) {
+          this.params[pi].input.value = parseFloat(value).toFixed(4)
+          this.params[pi].input.dispatchEvent(new Event('change'))
+        }
+      }
     }
 
     setTimeout(() => {
